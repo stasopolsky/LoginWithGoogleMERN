8d34842f5d5ec66f201722e46910c276f10fa805
Stas Opolsky <stas.opolsky@gmail.com>
2024-03-11 19:04:14 +0200

    init

---
+++ server/app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 6005;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json("server start");
+});
+
+app.listen(PORT, () => {
+  console.log(`server start at port ${PORT}`);
+});
